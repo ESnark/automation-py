b437a965a4a711455cedbe7dab8dbbee0fad27bf
ESnark <settimeout1000@gmail.com>
2022-04-17 20:07:54 +0900

    Fix: Read&write .csv files line by line

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+사업자번호로 과세여부를 조회하는 간단한 파이썬 프로그램입니다. 홈택스 서비스를 사용합니다.
 
+[selenium](https://pypi.org/project/selenium/), [webdriver-manager](https://pypi.org/project/webdriver-manager/)를 활용하여 만들어졌습니다.
+
+# 사용방법
+> 모든 입출력 csv 파일은 euc-kr로 인코딩됩니다.
+
+`main.exe`과 같은 위치에 **사업자번호.csv** 파일을 놓고 main.exe 파일을 실행시킵니다.
+
+사업자번호 조회 결과는 동일한 경로의 **output.csv** 파일로 생성됩니다.
+
+
+**사업자번호.csv 예시**
+```csv
+사업자번호,과세여부,조회일
+000-00-00000,,
+000-00-00000,,
+000-00-00000,,
+```
--- main.py
+++ main.py
@@ -1,68 +1,47 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[96]:
-
-import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
 from selenium.webdriver.common.by import By
-import pandas as pd
-
+import os
+import time
+import csv
 
 def main():
-  # In[97]:
-
-
   chrome_service = Service(ChromeDriverManager().install())
   chrome_options = webdriver.ChromeOptions()
   chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
   driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-
-  # In[98]:
-
-
   driver.get('https://teht.hometax.go.kr/websquare/websquare.html?w2xPath=/ui/ab/a/a/UTEABAAA13.xml')
 
+  working_dir = os.getcwd()
+  input_path = os.path.join(working_dir, '사업자번호.csv')
+  output_path = os.path.join(working_dir, 'output.csv')
 
-  # In[99]:
+  if os.path.exists(output_path):
+    os.remove(output_path)
 
+  with open(output_path, 'w', newline='\r\n' ,encoding='euc-kr') as output:
+    writer = csv.writer(output)
+    writer.writerow(['', '과세여부', '조회일'])
 
-  df = pd.read_csv('사업자번호.csv', encoding='euc-kr')
-  df.loc[:, '과세여부'] = df.loc[:, '과세여부'].astype('str')
-  df.loc[:, '조회일'] = df.loc[:, '조회일'].astype('str')
+    with open(input_path, encoding='euc-kr') as input:
+      next(input)
+      reader = csv.reader(input)
+      for row in reader:
+        business_key = row[0]
+        driver.find_element(By.ID, 'bsno').send_keys(business_key)
+        driver.find_element(By.ID, 'trigger5').click()
 
+        time.sleep(1)
+        table_body = driver.find_element(By.ID, 'grid2_body_tbody')
+        tmp = table_body.text.split(" ")
+        writer.writerow([tmp[0], " ".join(tmp[1:-1]), tmp[-1]])
 
-  # In[100]:
-
-
-  import time
-
-
-  # In[101]:
-
-
-  for i in range(len(df)):
-      time.sleep(5)
-      business_key = df.iloc[i]['사업자번호']
-      driver.find_element(By.ID, 'bsno').send_keys(business_key)
-      driver.find_element(By.ID, 'trigger5').click()
-      time.sleep(1)
-      table_body = driver.find_element(By.ID, 'grid2_body_tbody')
-      tmp = table_body.text.split(" ")
-      result = [tmp[0], " ".join(tmp[1:-1]), tmp[-1]]
-
-      df.at[i, '과세여부'] = result[1]
-      df.at[i, '조회일'] = result[2]
-
-
-  # In[102]:
-
-
-  df.to_csv('output.csv', encoding='euc-kr')
 
 if __name__ == '__main__':
   main()
